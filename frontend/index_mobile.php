3a8fbed304f9b3a903ed4afd0315722a7c9ba4aa
jjm61 <jjm61@cam.ac.uk>
2014-02-13 22:10:29 +0000

    mobile site, background and mobile switch

---
+++ frontend/index_mobile.php
@@ -0,0 +1,37 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width" />
+<title>Jesus May Ball 2014</title>
+<link href="./index_mobile.css" rel="stylesheet" type="text/css" />
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<!--<script src="parallax.js" type="text/javascript"></script>-->
+</head>
+
+<body>
+	<div id="mobileBody">
+		<div id="background"></div>
+		<img id="mobileLogo" src="media/logo.png" class="logo" alt="Loading..." />
+		<h1 class="mobileTitle">Jesus Mayball 2014</h1>
+			<p>Tickets will be available for Jesus College students from the 21st of February at 8am, for a week before general release on the 28th of February at 9pm.</p>
+				<h4>Standard</h4>
+				<p class="snug">- £127 + £2 -</p>
+				<!--<p class="snug">(SOLD OUT)</p>-->
+				<p class="snug">Enjoy unlimited food drinks and entertainment!</p>
+				<h4>Priority</h4>
+				<p class="snug">- £143 + £2 -</p>
+				<!--<p class="snug">(SOLD OUT)</p>-->
+				<p class="snug">Take advantage of early entry and jump the queue!</p>
+				<h4>Dining</h4>
+				<p class="snug">- £164 + £2 -</p>
+				<!--<p class="snug">(SOLD OUT)</p>-->
+				<p class="snug">Indulge in a champagne reception and a three course meal in Jesus Hall!</p>
+				<p>Book your ticket <a href="tickets">here</a>.</p>
+			<br/>
+			<p>Staffing applications can be found <a href="staff">here</a>.</p>
+			<br/>
+			<p>Ents applications can be found <a href="ents">here</a>.</p>
+			<br/><br/>
+			<p>Please visit our desktop site for more information.</p>
+			<br/>
+	</div>
+</body>
+</html>
